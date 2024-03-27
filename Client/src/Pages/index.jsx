1b5b9e49c83e4ef6bba84d80f9069e877989e0f2
Naman Goel <namangoel781@gmail.com>
2024-03-27 20:20:50 +0530

    link added

--- Client/src/Pages/index.jsx
+++ Client/src/Pages/index.jsx
@@ -18,7 +18,7 @@ export default function Index() {
               {place.photos?.[0] && (
                 <img
                   className="rounded-2xl object-cover aspect-square"
-                  src={"http://localhost:4000/uploads/" + place.photos?.[0]}
+                  src={"https://airbnb-1i6s.onrender.com/uploads/" + place.photos?.[0]}
                   alt=""
                 />
               )}
